458d8cdd63a320ebdfc7fa88570d3abd9b9c1424
Lucas Aschenbach <37406743+LucasAschenbach@users.noreply.github.com>
2022-04-27 13:52:16 +0200

    Add SliverAlignment to exports

--- lib/sliver_tools.dart
+++ lib/sliver_tools.dart
@@ -7,3 +7,5 @@ export 'src/sliver_cross_axis_constrained.dart';
 export 'src/sliver_cross_axis_padded.dart';
 export 'src/sliver_pinned_header.dart';
 export 'src/sliver_stack.dart';
+
+export 'src/types/sliver_alignment.dart';
