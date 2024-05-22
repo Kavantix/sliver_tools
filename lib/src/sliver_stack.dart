7eb2ad4c1948ef2cf3eba01c03efbc3297775d09
LinXunFeng <linxunfeng@yeah.net>
2024-05-22 10:58:04 +0800

    Removed redundant of

--- lib/src/sliver_stack.dart
+++ lib/src/sliver_stack.dart
@@ -8,7 +8,7 @@ import 'rendering/sliver_stack.dart';
 /// Its children can either be slivers or box children that are positioned
 /// using [SliverPositioned] with at least one of its values not null
 /// This means that only the sliver children have an effect on the size of
-/// of this sliver and the box children are meant to follow the slivers
+/// this sliver and the box children are meant to follow the slivers
 class SliverStack extends MultiChildRenderObjectWidget {
   // flutter pre 3.13 does not allow the constructor to be const
   // ignore: prefer_const_constructors_in_immutables
